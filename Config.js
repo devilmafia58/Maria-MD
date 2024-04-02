bc762131c1ddd828b8fa461bcc9b7a469047feca
devilmafia58 <165783868+devilmafia58@users.noreply.github.com>
2024-04-02 16:04:53 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['8294985178']
+global.ownername = "ʀᴀᴅʜɪᴋᴀ"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
